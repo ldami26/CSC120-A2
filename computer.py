ffdeb224fa408d8da10f04b9b0f2e479ec427019
ldami26 <ldamier@smith.edu>
2024-09-22 16:39:24 -0400

    commit

--- computer.py
+++ computer.py
@@ -1,25 +1,14 @@
 class Computer:
-
-    # What attributes will it need?
-    computer_id: str
-    description: str 
-    processor_type: str
-    hard_drive_capacity: int
-    memory: int
-    operating_system: str
-    year_made: int
-    price: int
-    
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
+    # Attributes
     def __init__(self, computer_id: str,
-                    description: str,
-                    processor_type: str,
-                    hard_drive_capacity: int,
-                    memory: int,
-                    operating_system: str,
-                    year_made: int,
-                    price: int):
+                 description: str,
+                 processor_type: str,
+                 hard_drive_capacity: int,
+                 memory: int,
+                 operating_system: str,
+                 year_made: int,
+                 price: int):
+        # Initializing attributes
         self.computer_id = computer_id
         self.description = description
         self.processor_type = processor_type
@@ -28,34 +17,62 @@ class Computer:
         self.operating_system = operating_system
         self.year_made = year_made
         self.price = price
-         # You'll remove this when you fill out your constructor
 
-    # What methods will you need?
+    # String representation of the object
     def __str__(self) -> str:
-        return "Desc: " + str(self.description) + "\n" + "Processor:" + str(self.processor_type) + "\n"+ "Hard Drive:" + str(self.hard_drive_capacity) + "\n"
+        return (f"Description: {self.description}\n"
+                f"Processor: {self.processor_type}\n"
+                f"Hard Drive: {self.hard_drive_capacity} GB\n"
+                f"Memory: {self.memory} GB\n"
+                f"Operating System: {self.operating_system}\n"
+                f"Year Made: {self.year_made}\n"
+                f"Price: ${self.price}")
 
-def update_price(self, new_price: float):
+    # Method to update the price
+    def update_price(self, new_price: float):
         """Update the price of the computer"""
         self.price = new_price
-        print(f"Updated price for computer ID {self.computer_id} to {self.price}")
+        print(f"Updated price for computer ID {self.computer_id} to ${self.price}")
 
-def update_os(self, new_os: str):
+    # Method to update the operating system
+    def update_os(self, new_os: str):
         """Update the OS of the computer"""
-        self.os = new_os
-        print(f"Updated OS for computer ID {self.computer_id} to {self.os}")
-    
-def get_info(self):
+        self.operating_system = new_os
+        print(f"Updated OS for computer ID {self.computer_id} to {self.operating_system}")
+
+    # Method to get full information about the computer
+    def get_info(self):
         """Get a string representation of the computer's info"""
-        return f"ID: {self.computer_id}, Description: {self.description}, Processor: {self.processor}, Memory: {self.memory}GB, Storage: {self.storage}GB, OS: {self.os}, Year: {self.year_made}, Price: ${self.price}"
+        return (f"ID: {self.computer_id}, Description: {self.description}, "
+                f"Processor: {self.processor_type}, Memory: {self.memory} GB, "
+                f"Storage: {self.hard_drive_capacity} GB, OS: {self.operating_system}, "
+                f"Year: {self.year_made}, Price: ${self.price}")
 
-
+# Main function to create and test the Computer object
 def main():
+    # Creating a Computer object
     my_computer = Computer(
-        "Mac Pro (Late 2013)",
-        "3.5 GHc 6-Core Intel Xeon E5",
-        1024, 64,
-        "macOS Big Sur", 2013, 1500
+        "C001",  # computer_id
+        "Mac Pro (Late 2013)",  # description
+        "3.5 GHz 6-Core Intel Xeon E5",  # processor_type
+        1024,  # hard_drive_capacity
+        64,  # memory
+        "macOS Big Sur",  # operating_system
+        2013,  # year_made
+        1500  # price
     )
-    print("Description:", my_computer.description)
-    
+
+    # Display the computer description
+    print(my_computer)  # using __str__ method
+
+    # Update the price
+    my_computer.update_price(1200)
+
+    # Update the operating system
+    my_computer.update_os("macOS Monterey")
+
+    # Get the updated computer info
+    print(my_computer.get_info())
+
+# Run the main function
 main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,83 +1,112 @@
+from typing import Dict, Optional
+
 class ResaleShop:
-
-    # What attributes will it need?
-
     # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        """Initialize the resale shop with an empty inventory"""
-        self.inventory = {}  # Inventory will store computers with their IDs as keys
+    def __init__(self):
+        """Initialize the resale shop with an empty inventory and item ID counter"""
+        self.inventory: Dict[int, Dict] = {}  # Inventory will store computers with their IDs as keys
+        self.itemID: int = 0  # Item ID counter
 
-    # What methods will you need?
+    """
+    Takes in a Dict containing all the information about a computer,
+    adds it to the inventory, returns the assigned item_id
+    """
+    def buy(self, computer: Dict):
+        self.itemID += 1  # increment itemID
+        self.inventory[self.itemID] = computer  # add to inventory
+        return self.itemID
 
-    inventory : Dict[int, Dict] = {}
-
-itemID = 0 # We'll increment this every time we add a new item 
-           # so that we always have a new value for the itemID
-
-"""
-Takes in a Dict containing all the information about a computer,
-adds it to the inventory, returns the assigned item_id
-"""
-def buy(computer: Dict):
-    global itemID
-    itemID += 1 # increment itemID
-    inventory[itemID] = computer
-    return itemID
-
-"""
-Takes in an item_id and a new price, updates the price of the associated
-computer if it is the inventory, prints error message otherwise
-"""
-def update_price(item_id: int, new_price: int):
-    if item_id in inventory:
-        inventory[item_id]["price"] = new_price
-    else:
-        print("Item", item_id, "not found. Cannot update price.")
-
-"""
-Takes in an item_id, removes the associated computer if it is the inventory, 
-prints error message otherwise
-"""
-def sell(item_id: int):
-    if item_id in inventory:
-        del inventory[item_id]
-        print("Item", item_id, "sold!")
-    else: 
-        print("Item", item_id, "not found. Please select another item to sell.")
-
-"""
-prints all the items in the inventory (if it isn't empty), prints error otherwise
-"""
-def print_inventory():
-    # If the inventory is not empty
-    if inventory:
-        # For each item
-        for item_id in inventory:
-            # Print its details
-            print(f'Item ID: {item_id} : {inventory[item_id]}')
-    else:
-        print("No inventory to display.")
-
-def refurbish(item_id: int, new_os: Optional[str] = None):
-    if item_id in inventory:
-        computer = inventory[item_id] # locate the computer
-        if int(computer["year_made"]) < 2000:
-            computer["price"] = 0 # too old to sell, donation only
-        elif int(computer["year_made"]) < 2012:
-            computer["price"] = 250 # heavily-discounted price on machines 10+ years old
-        elif int(computer["year_made"]) < 2018:
-            computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+    """
+    Takes in an item_id and a new price, updates the price of the associated
+    computer if it is the inventory, prints error message otherwise
+    """
+    def update_price(self, item_id: int, new_price: int):
+        if item_id in self.inventory:
+            self.inventory[item_id]["price"] = new_price
+            print(f"Updated price for item {item_id} to ${new_price}")
         else:
-            computer["price"] = 1000 # recent stuff
+            print(f"Item {item_id} not found. Cannot update price.")
 
-        if new_os is not None:
-            computer["operating_system"] = new_os # update details after installing new OS
-    else:
-        print("Item", item_id, "not found. Please select another item to refurbish.")
+    """
+    Takes in an item_id, removes the associated computer if it is the inventory, 
+    prints error message otherwise
+    """
+    def sell(self, item_id: int):
+        if item_id in self.inventory:
+            del self.inventory[item_id]  # remove item from inventory
+            print(f"Item {item_id} sold!")
+        else:
+            print(f"Item {item_id} not found. Please select another item to sell.")
 
+    """
+    Prints all the items in the inventory (if it isn't empty), prints error otherwise
+    """
+    def print_inventory(self):
+        # If the inventory is not empty
+        if self.inventory:
+            # For each item
+            for item_id, computer in self.inventory.items():
+                # Print its details
+                print(f'Item ID: {item_id} : {computer}')
+        else:
+            print("No inventory to display.")
+
+    """
+    Refurbish a computer: update price based on year made, optionally update the OS
+    """
+    def refurbish(self, item_id: int, new_os: Optional[str] = None):
+        if item_id in self.inventory:
+            computer = self.inventory[item_id]  # locate the computer
+            year_made = int(computer["year_made"])
+
+            # Update price based on the year made
+            if year_made < 2000:
+                computer["price"] = 0  # too old to sell, donation only
+            elif year_made < 2012:
+                computer["price"] = 250  # heavily-discounted price on machines 10+ years old
+            elif year_made < 2018:
+                computer["price"] = 550  # discounted price on machines 4-to-10 year old machines
+            else:
+                computer["price"] = 1000  # recent stuff
+
+            # Optionally update the OS if a new OS is provided
+            if new_os is not None:
+                computer["operating_system"] = new_os
+
+            print(f"Item {item_id} has been refurbished. New price: ${computer['price']}")
+        else:
+            print(f"Item {item_id} not found. Please select another item to refurbish.")
+
+# Main function to test the ResaleShop functionality
 def main():
-    buy({"description":"2019 MacBook Pro", "processor_type":"Intel", "hard_drive_capacity":256, "memory":16, "operating_system":"High Sierra", "year_made":2019, "price":1000})
-    print_inventory()
+    # Create a new ResaleShop instance
+    shop = ResaleShop()
 
+    # Add a computer to the inventory
+    item_id = shop.buy({
+        "description": "2019 MacBook Pro",
+        "processor_type": "Intel",
+        "hard_drive_capacity": 256,
+        "memory": 16,
+        "operating_system": "High Sierra",
+        "year_made": 2019,
+        "price": 1000
+    })
+
+    # Print the inventory
+    shop.print_inventory()
+
+    # Update the price of the computer
+    shop.update_price(item_id, 900)
+
+    # Refurbish the computer with a new OS
+    shop.refurbish(item_id, "macOS Monterey")
+
+    # Sell the computer
+    shop.sell(item_id)
+
+    # Try to print the inventory after the sale
+    shop.print_inventory()
+
+# Run the main function
 main()
